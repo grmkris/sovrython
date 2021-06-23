9d78e8e2966058686ad09651ca5f0c3ef5a561db
grmkris <kristjan.grm1@gmail.com>
2021-06-23 12:59:22 +0200

    removed unnecessary stuff

--- rsk-next/contracts/TimeStamp.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity >=0.4.21 <0.7.0;
-
-contract TimeStamp {
-  function Time_call() public view returns (uint256){
-    return now;
-  }
-}
--- rsk-next/migrations/4_deploy_contracts.js
+++
@@ -1,5 +0,0 @@
-const TimeStamp = artifacts.require("TimeStamp");
-
-module.exports = function(deployer) {
-  deployer.deploy(TimeStamp);
-};
