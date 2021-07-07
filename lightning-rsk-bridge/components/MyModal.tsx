bdbbeef2e13b37f2bb2b7061781c4e9aca1628ee
grmkris <kristjan.grm1@gmail.com>
2021-07-07 23:11:39 +0200

    now using only api as string

--- lightning-rsk-bridge/components/MyModal.tsx
+++ lightning-rsk-bridge/components/MyModal.tsx
@@ -4,7 +4,7 @@ import Loader from "react-loader-spinner";
 import {CreateInvoice} from "../interfaces";
 import Web3 from "web3";
 
-const API_URL = process.env.VERCEL_URL + "api";
+const API_URL = "api";
 
 declare let window: any;
 const QRCode = require('qrcode.react');
